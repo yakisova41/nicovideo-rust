e968d0ae6678a031f3ba7b83931b9029ba1f76d3
yakisova41 <yakisoba5emon@gmail.com>
2024-02-25 00:28:29 +0900

    🐛 fix implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 mod initial_watch_data;
 mod session;
 
+
 pub mod nicovideo_rust {
+    use serde_derive::Deserialize;
+    use serde_derive::Serialize;
+
     use crate::{initial_watch_data::{self, InitialWatchData}, session::{self, SessionResponse}};
 
     #[allow(dead_code)]
-    #[derive(Clone)]
     #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
     pub struct NicoSession {
         pub session: SessionResponse,
